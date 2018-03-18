0193f72ac03a79b3d883380248ace3466cf16e2d
Caique Siqueira <cslclaman@gmail.com>
2018-03-18 01:01:48 -0300

    Adiciona enums para cada tipo de variável e para delimitador ponto

--- src/alog/model/FuncaoToken.java
+++ src/alog/model/FuncaoToken.java
@@ -10,15 +10,19 @@ package alog.model;
  * @author Caique
  */
 public enum FuncaoToken {
+    _INDEFINIDO,
     //Palavras reservadas - início e fim de bloco
     RES_BLOCO_INICIO,
     RES_BLOCO_FIM,
     //Identificadores de variável
-    IDENT_TIPO_VARIAVEL,
+    IDENT_TIPO_INTEIRO,
+    IDENT_TIPO_REAL,
+    IDENT_TIPO_CARACTER,
     IDENT_NOME_VARIAVEL,
     //Delimitadores
     DELIM_DOIS_PONTOS,
     DELIM_PONTO_VIRGULA,
+    DELIM_PONTO,
     DELIM_VIRGULA,
     DELIM_PARENTESES_ABRE,
     DELIM_PARENTESES_FECHA,
