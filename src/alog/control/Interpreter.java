869db898943b8039d67a732fdb452eec26b8530d
Caique Siqueira <cslclaman@gmail.com>
2018-03-19 00:43:21 -0300

    Separa execuções do Interpreter em métodos. Adiciona rotina de leitura.

--- src/alog/control/Interpreter.java
+++ src/alog/control/Interpreter.java
@@ -9,6 +9,7 @@ import alog.model.Expressao;
 import alog.model.TipoVariavel;
 import alog.model.Token;
 import alog.model.Variavel;
+import java.util.InputMismatchException;
 import java.util.LinkedList;
 
 /**
@@ -29,38 +30,106 @@ public class Interpreter {
         listaVariaveis = new LinkedList<>();
     }
     
-    public void executa(Expressao expressao){
+    public boolean executa(Expressao expressao){
         switch(expressao.getTipo()){
             case DELIM_BLOCO:
-                switch (expressao.getTokenAt(0).getFuncaoToken()){
-                    case RES_BLOCO_INICIO:
-                        bloco ++;
-                        break;
-                    case RES_BLOCO_FIM:
-                        bloco --;
-                        break;
-                }
-                break;
+                return execDelimBloco(expressao);
             case CRIACAO_VARIAVEL:
-                TipoVariavel tipoVar;
-                switch (expressao.getTokenAt(0).getFuncaoToken()){
-                    case IDENT_TIPO_INTEIRO:
-                        tipoVar = TipoVariavel.INTEIRO;
-                        break;
-                    case IDENT_TIPO_REAL:
-                        tipoVar = TipoVariavel.REAL;
-                        break;
-                    case IDENT_TIPO_CARACTER:
-                    default:
-                        tipoVar = TipoVariavel.CARACTER;
-                        break;
-                }
-                for (int it = 1; it < expressao.getNumTokens(); it++){
-                    Token token = expressao.getTokenAt(it);
-                    Variavel variavel = new Variavel(tipoVar, token.getPalavra());
-                    listaVariaveis.add(variavel);
-                }
+                return execCriacaoVariavel(expressao);
+            case ENTRADA_DE_DADOS:
+                return execEntradaDados(expressao);
+        }
+        return false;
+    }
+    
+    public boolean execDelimBloco(Expressao expressao){
+        switch (expressao.getNext().getFuncaoToken()){
+            case RES_BLOCO_INICIO:
+                bloco ++;
+                break;
+            case RES_BLOCO_FIM:
+                bloco --;
                 break;
         }
+        return true;
+    }
+    
+    public boolean execCriacaoVariavel(Expressao expressao){
+        TipoVariavel tipoVar;
+        switch (expressao.getNext().getFuncaoToken()){
+            case IDENT_TIPO_INTEIRO:
+                tipoVar = TipoVariavel.INTEIRO;
+                break;
+            case IDENT_TIPO_REAL:
+                tipoVar = TipoVariavel.REAL;
+                break;
+            case IDENT_TIPO_CARACTER:
+                tipoVar = TipoVariavel.CARACTER;
+                break;
+            default:
+                return false;
+        }
+        while (expressao.hasNext()){
+            Variavel variavel = new Variavel(tipoVar, expressao.getNext().getPalavra());
+            listaVariaveis.add(variavel);
+        }
+        
+        return true;
+    }
+    
+    public boolean execEntradaDados(Expressao expressao){
+        expressao.setIndice(1);
+        while (expressao.hasNext()){
+            int indiceVar = pesquisaVariavel(expressao.getNext().getPalavra());
+            if (indiceVar >= 0){
+                Variavel variavel = listaVariaveis.get(indiceVar);
+                System.out.println("Valor para a variável " + variavel.getNome() + " (" + variavel.getTipo() + ")");
+                java.util.Scanner sc = new java.util.Scanner(System.in);
+                
+                boolean valorValido = true;
+                do {
+                    valorValido = true;
+                    switch(variavel.getTipo()){
+                        case CARACTER:
+                            variavel.setValor(sc.nextLine());
+                            break;
+                        case INTEIRO:
+                            try {
+                                int valor = sc.nextInt();
+                                variavel.setValor(String.valueOf(valor));
+                            } catch (InputMismatchException ex){
+                                System.out.println("Valor inválido - esperado valor inteiro");
+                                valorValido = false;
+                            }
+                            break;
+                        case REAL:
+                            try {
+                                double valor = sc.nextDouble();
+                                variavel.setValor(String.valueOf(valor));
+                            } catch (InputMismatchException ex){
+                                System.out.println("Valor inválido - esperado valor real");
+                                valorValido = false;
+                            }
+                            break;
+                    }
+                } while (!valorValido);
+                
+                listaVariaveis.set(indiceVar, variavel);
+            }
+        }
+        
+        return true;
+    }
+    
+    private int pesquisaVariavel(String nome){
+        int indice = 0;
+        for (Variavel v : listaVariaveis){
+            if (v.getNome().equals(nome)){
+                return indice;
+            } else {
+                indice ++;
+            }
+        }
+        return -1;
     }
 }
