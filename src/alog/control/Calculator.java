0582a6cd8f5bf5612f7894f5dfc81ddc043a34ea
Caique Siqueira <cslclaman@gmail.com>
2018-03-20 00:43:53 -0300

    Implementa rotinas para todas as operações aritméticas.

--- src/alog/control/Calculator.java
+++ src/alog/control/Calculator.java
@@ -24,11 +24,73 @@ public class Calculator {
         funcao = FuncaoToken._INDEFINIDO;
     }
     
-    public Token executaOperacaoBinaria(Variavel op1, Variavel op2){
+    public Token executaOperacaoAritmetica(Variavel op1, Variavel op2){
         if (!defineOperacao(op1, op2)){
             return null;
         } else {
-            return operador;
+            String result = "";
+        
+            switch (operador.getFuncaoToken()){
+                case OP_SOMA:
+                    switch (funcao){
+                        case CONST_INTEIRA:
+                            result = String.valueOf(op1.getValorInteiro() + op2.getValorInteiro());
+                            break;
+                        case CONST_REAL:
+                            result = String.valueOf(op1.getValorReal()+ op2.getValorReal());
+                            break;
+                    }
+                    break;
+                case OP_SUBTRACAO:
+                    switch (funcao){
+                        case CONST_INTEIRA:
+                            result = String.valueOf(op1.getValorInteiro() - op2.getValorInteiro());
+                            break;
+                        case CONST_REAL:
+                            result = String.valueOf(op1.getValorReal() - op2.getValorReal());
+                            break;
+                    }
+                    break;
+                case OP_MULTIPLICACAO:
+                    switch (funcao){
+                        case CONST_INTEIRA:
+                            result = String.valueOf(op1.getValorInteiro() * op2.getValorInteiro());
+                            break;
+                        case CONST_REAL:
+                            result = String.valueOf(op1.getValorReal() * op2.getValorReal());
+                            break;
+                    }
+                    break;
+                case OP_DIV_INTEIRA:
+                    if (op2.getValorInteiro() == 0){
+                        System.err.println("Erro: Divisão por Zero");
+                        return null;
+                    }
+                    result = String.valueOf(op1.getValorInteiro() / op2.getValorInteiro());
+                    break;
+                case OP_MOD:
+                    if (op2.getValorInteiro() == 0){
+                        System.err.println("Erro: Divisão por Zero");
+                        return null;
+                    }
+                    result = String.valueOf(op1.getValorInteiro() % op2.getValorInteiro());
+                    break;
+                case OP_DIV_REAL:
+                    if (op2.getValorReal() == 0.0){
+                        System.err.println("Erro: Divisão por Zero");
+                        return null;
+                    }
+                    result = String.valueOf(op1.getValorReal() / op2.getValorReal());
+                    break;
+            }
+            if (!result.isEmpty()){
+                operador.setPalavra(result);
+                operador.setFuncaoToken(funcao);
+                return operador;
+            } else {
+                System.err.println("Erro ao calcular - sem resultado");
+                return null;
+            }
         }
     } 
     
@@ -48,6 +110,41 @@ public class Calculator {
                     }
                 }
                 break;
+            case OP_DIV_INTEIRA:
+            case OP_MOD:
+                if (op1.getTipo() == TipoVariavel.CARACTER || op2.getTipo() == TipoVariavel.CARACTER){
+                    System.err.println("Operação " + operador.getFuncaoToken()+ " inválida para tipo de dado Caracter");
+                    return false;
+                } else {
+                    if (op1.getTipo() == TipoVariavel.INTEIRO && op2.getTipo() == op1.getTipo()){
+                        funcao = FuncaoToken.CONST_INTEIRA;
+                    } else {
+                        System.err.println("Operação " + operador.getFuncaoToken()+ " inválida para tipo de dado Real");
+                        return false;
+                    }
+                }
+                break;
+            case OP_DIV_REAL:
+                if (op1.getTipo() == TipoVariavel.CARACTER || op2.getTipo() == TipoVariavel.CARACTER){
+                    System.err.println("Operação " + operador.getFuncaoToken()+ " inválida para tipo de dado Caracter");
+                    return false;
+                } else {
+                    if (op1.getTipo() == TipoVariavel.INTEIRO && op2.getTipo() == op1.getTipo()){
+                        System.err.println("Operação " + operador.getFuncaoToken()+ " inválida entre tipos de dados Inteiros");
+                        return false;
+                    } else {
+                        funcao = FuncaoToken.CONST_REAL;
+                    }
+                }
+                break;
+        }
+        if (!op1.isInicializada()){
+            System.err.println("Variável " + op1.getNome() + " não inicializada");
+            return false;
+        } 
+        if (!op2.isInicializada()){
+            System.err.println("Variável " + op2.getNome() + " não inicializada");
+            return false;
         }
         return true;
     }
