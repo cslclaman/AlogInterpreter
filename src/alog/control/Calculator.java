042906505bebf406cb287863f2c512f5833896ef
Caique Siqueira <cslclaman@gmail.com>
2018-03-19 21:15:24 -0300

    Adiciona classe para executar operações aritméticas

---
+++ src/alog/control/Calculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alog.control;
+
+import alog.model.Token;
+import alog.model.Variavel;
+
+/**
+ *
+ * @author Caique Souza
+ */
+public class Calculator {
+    private Token operador;
+    
+    public Calculator(Token operador){
+        this.operador = operador;
+    }
+    
+    public Token executaOperacaoBinaria(Variavel op1, Variavel op2){
+        
+    } 
+}
