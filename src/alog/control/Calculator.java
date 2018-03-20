0b577ccb94e7d035d417ce4cde9c468aee3971e4
Caique Souza <caique.siqueira@fatec.sp.gov.br>
2018-03-19 22:18:48 -0300

    Adiciona rotina de verificação de tipos de variáveis

--- src/alog/control/Calculator.java
+++ src/alog/control/Calculator.java
@@ -5,6 +5,9 @@
  */
 package alog.control;
 
+import alog.model.FuncaoToken;
+import alog.model.TipoToken;
+import alog.model.TipoVariavel;
 import alog.model.Token;
 import alog.model.Variavel;
 
@@ -14,12 +17,38 @@ import alog.model.Variavel;
  */
 public class Calculator {
     private Token operador;
+    private FuncaoToken funcao;
     
     public Calculator(Token operador){
         this.operador = operador;
+        funcao = FuncaoToken._INDEFINIDO;
     }
     
     public Token executaOperacaoBinaria(Variavel op1, Variavel op2){
-        
+        if (!defineOperacao(op1, op2)){
+            return null;
+        } else {
+            return operador;
+        }
     } 
+    
+    private boolean defineOperacao(Variavel op1, Variavel op2){
+        switch (operador.getFuncaoToken()){
+            case OP_SOMA:
+            case OP_SUBTRACAO:
+            case OP_MULTIPLICACAO:
+                if (op1.getTipo() == TipoVariavel.CARACTER || op2.getTipo() == TipoVariavel.CARACTER){
+                    System.err.println("Operação " + operador.getFuncaoToken()+ " inválida para tipo de dado Caracter");
+                    return false;
+                } else {
+                    if (op1.getTipo() == TipoVariavel.INTEIRO && op2.getTipo() == op1.getTipo()){
+                        funcao = FuncaoToken.CONST_INTEIRA;
+                    } else {
+                        funcao = FuncaoToken.CONST_REAL;
+                    }
+                }
+                break;
+        }
+        return true;
+    }
 }
