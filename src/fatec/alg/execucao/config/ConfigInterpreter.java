7f06aeccfa080811deef084da001778fa5bb3cf2
Caique Siqueira <cslclaman@gmail.com>
2018-10-30 18:53:00 -0200

    Adiciona configurações para finalização de expressões e loop para-faça

--- src/fatec/alg/execucao/config/ConfigInterpreter.java
+++ src/fatec/alg/execucao/config/ConfigInterpreter.java
@@ -19,6 +19,7 @@ public class ConfigInterpreter {
     public final static String RUNNEXT_LEIA_ATRIB = "runnext.leia.atrib";
     public final static String RUNNEXT_ESCREVA_PILHA = "runnext.escreva.pilha";
     public final static String RUNNEXT_EXPR_PILHA = "runnext.expressao.pilha";
+    public final static String RUNNEXT_PARA_LOOP = "runnext.para.loop";
     public final static String RUNNEXT_EXPR_EXEC_CONST = "runnext.expressao.exec.constante";
     public final static String RUNNEXT_EXPR_EXEC_VAR = "runnext.expressao.exec.variavel";
     public final static String RUNNEXT_EXPR_EXEC_FUNC = "runnext.expressao.exec.funcao";
@@ -29,6 +30,11 @@ public class ConfigInterpreter {
     public final static String RUNNEXT_EXPR_RES_FUNC = "runnext.expressao.result.funcao";
     public final static String RUNNEXT_EXPR_RES_UNARIA = "runnext.expressao.result.opunaria";
     public final static String RUNNEXT_EXPR_RES_OPBIN = "runnext.expressao.result.opbinaria";
+    public final static String RUNNEXT_EXPR_FIN_CONST = "runnext.expressao.final.constante";
+    public final static String RUNNEXT_EXPR_FIN_VAR = "runnext.expressao.final.variavel";
+    public final static String RUNNEXT_EXPR_FIN_FUNC = "runnext.expressao.final.funcao";
+    public final static String RUNNEXT_EXPR_FIN_UNARIA = "runnext.expressao.final.opunaria";
+    public final static String RUNNEXT_EXPR_FIN_OPBIN = "runnext.expressao.final.opbinaria";
     public final static String FORMAT_ESCREVA_QUEBRA = "format.escreva.quebralinha";
     
     public ConfigInterpreter(){
@@ -39,16 +45,25 @@ public class ConfigInterpreter {
         configs.put(RUNNEXT_ESCREVA_PILHA, false);
         configs.put(FORMAT_ESCREVA_QUEBRA, true);
         configs.put(RUNNEXT_EXPR_PILHA, true);
+        configs.put(RUNNEXT_PARA_LOOP, true);
+        
         configs.put(RUNNEXT_EXPR_EXEC_CONST, false);
-        configs.put(RUNNEXT_EXPR_RES_CONST, true);
         configs.put(RUNNEXT_EXPR_EXEC_VAR, false);
-        configs.put(RUNNEXT_EXPR_RES_VAR, false);
         configs.put(RUNNEXT_EXPR_EXEC_FUNC, false);
-        configs.put(RUNNEXT_EXPR_RES_FUNC, true);
         configs.put(RUNNEXT_EXPR_EXEC_UNARIA, false);
-        configs.put(RUNNEXT_EXPR_RES_UNARIA, false);
         configs.put(RUNNEXT_EXPR_EXEC_OPBIN, false);
+        
+        configs.put(RUNNEXT_EXPR_RES_CONST, true);
+        configs.put(RUNNEXT_EXPR_RES_VAR, false);
+        configs.put(RUNNEXT_EXPR_RES_FUNC, true);
+        configs.put(RUNNEXT_EXPR_RES_UNARIA, false);
         configs.put(RUNNEXT_EXPR_RES_OPBIN, false);
+        
+        configs.put(RUNNEXT_EXPR_FIN_CONST, true);
+        configs.put(RUNNEXT_EXPR_FIN_VAR, false);
+        configs.put(RUNNEXT_EXPR_FIN_FUNC, false);
+        configs.put(RUNNEXT_EXPR_FIN_UNARIA, false);
+        configs.put(RUNNEXT_EXPR_FIN_OPBIN, false);
     }
     
     public boolean getBoolean(String key){
