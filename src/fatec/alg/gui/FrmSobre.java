c46d65cbde128e6ff8915d2a33cba66f26a02ca4
Caique Siqueira <caique.siqueira@fatec.sp.gov.br>
2018-11-10 20:52:20 -0200

    Renomeação de atributos e refatoramento de constantes

--- src/fatec/alg/gui/FrmSobre.java
+++ src/fatec/alg/gui/FrmSobre.java
@@ -10,20 +10,32 @@ import java.awt.Desktop;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
+import java.io.IOException;
 import java.net.URI;
-import javax.swing.JOptionPane;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author alani
+ * @author Caique Siqueira
  */
 public class FrmSobre extends javax.swing.JFrame {
+
+    private final static String URL_HOSPEDAGEM = "https://github.com/cslclaman/AlogInterpreter";
+    private final static String URL_CONTATO_EMAIL = "interpretador-alog-fatecso@outlook.com.br";
 
     /**
      * Creates new form FrmSobre
      */
     public FrmSobre() {
         initComponents();
+        this.setIconImage(new javax.swing.ImageIcon(
+                getClass().getResource("/fatec/alg/gui/imagens/logo-interpr-64.png"))
+                .getImage()
+        );
+        lblHospedagem.setText(lblHospedagem.getText().trim() + " " + URL_HOSPEDAGEM);
+        txtEmail.setText(URL_CONTATO_EMAIL);
     }
 
     /**
@@ -35,69 +47,68 @@ public class FrmSobre extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+        lblFaculdadeCurso = new javax.swing.JLabel();
+        lblLogoFaculdade = new javax.swing.JLabel();
+        lblNomeProjeto = new javax.swing.JLabel();
+        lblSobreProjeto = new javax.swing.JLabel();
+        lblNomeOrientador = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jLabel8 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
+        txtEmail = new javax.swing.JTextField();
+        btnFechar = new javax.swing.JButton();
+        lblHospedagem = new javax.swing.JLabel();
+        lblVersao = new javax.swing.JLabel();
+        btnCopiarEmail = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Sobre o programa");
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/fatec/alg/gui/imagens/logo-fatecso-64.png"))); // NOI18N
+        lblFaculdadeCurso.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
+        lblFaculdadeCurso.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblFaculdadeCurso.setText("Fatec Sorocaba - Curso de Análise e Desenvolvimento de Sistemas");
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("Interpretador de Algoritmos");
+        lblLogoFaculdade.setIcon(new javax.swing.ImageIcon(getClass().getResource("/fatec/alg/gui/imagens/logo-fatecso-64.png"))); // NOI18N
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("Fatec Sorocaba - Curso de Análise e Desenvolvimento de Sistemas");
+        lblNomeProjeto.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        lblNomeProjeto.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblNomeProjeto.setText("Interpretador de Algoritmos");
 
-        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel4.setText("Trabalho de Graduação do aluno Caíque de Souza Lima Siqueira");
+        lblSobreProjeto.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblSobreProjeto.setText("Trabalho de Graduação do aluno Caíque de Souza Lima Siqueira");
 
-        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel5.setText("Orientador: Prof. Dimas Ferreira Cardoso");
+        lblNomeOrientador.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblNomeOrientador.setText("Orientador: Prof. Dimas Ferreira Cardoso");
 
         jLabel6.setText("Contato: ");
 
-        jTextField1.setEditable(false);
-        jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jTextField1.setText("interpretador-alog-fatecso@outlook.com.br");
+        txtEmail.setEditable(false);
+        txtEmail.setHorizontalAlignment(javax.swing.JTextField.CENTER);
 
-        jButton1.setText("Fechar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnFechar.setText("Fechar");
+        btnFechar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnFecharActionPerformed(evt);
             }
         });
 
-        jLabel8.setForeground(new java.awt.Color(0, 0, 255));
-        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel8.setText("Projeto hospedado em https://github.com/cslclaman/AlogInterpreter");
-        jLabel8.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jLabel8.addMouseListener(new java.awt.event.MouseAdapter() {
+        lblHospedagem.setForeground(new java.awt.Color(0, 0, 255));
+        lblHospedagem.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblHospedagem.setText("Projeto hospedado em ENDERECO");
+        lblHospedagem.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblHospedagem.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabel8MouseClicked(evt);
+                lblHospedagemMouseClicked(evt);
             }
         });
 
-        jLabel7.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel7.setText(String.format("Versão %s (%s) - build %d", Principal.VERSAO_NUM, Principal.VERSAO_DATA, Principal.VERSAO_BUILD));
+        lblVersao.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        lblVersao.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblVersao.setText(String.format("Versão %s (%s) - build %d", Principal.VERSAO_NUM, Principal.VERSAO_DATA, Principal.VERSAO_BUILD));
 
-        jButton2.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
-        jButton2.setText("Copiar");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnCopiarEmail.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
+        btnCopiarEmail.setText("Copiar");
+        btnCopiarEmail.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnCopiarEmailActionPerformed(evt);
             }
         });
 
@@ -110,26 +121,26 @@ public class FrmSobre extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel8, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(lblFaculdadeCurso, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblNomeProjeto, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblVersao, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblHospedagem, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblSobreProjeto, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblNomeOrientador, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel1))
+                        .addComponent(lblLogoFaculdade))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel6)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField1))
+                                .addComponent(txtEmail))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(208, 208, 208)
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(btnFechar, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 8, Short.MAX_VALUE)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnCopiarEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(126, 126, 126)))
                 .addContainerGap())
         );
@@ -137,27 +148,27 @@ public class FrmSobre extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel3)
+                .addComponent(lblFaculdadeCurso)
                 .addGap(21, 21, 21)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(lblNomeProjeto, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel7)
+                        .addComponent(lblVersao)
                         .addGap(18, 18, 18)
-                        .addComponent(jLabel4))
-                    .addComponent(jLabel1))
+                        .addComponent(lblSobreProjeto))
+                    .addComponent(lblLogoFaculdade))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel5)
+                .addComponent(lblNomeOrientador)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel8)
+                .addComponent(lblHospedagem)
                 .addGap(27, 27, 27)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnCopiarEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(27, 27, 27)
-                .addComponent(jButton1)
+                .addComponent(btnFechar)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -165,37 +176,40 @@ public class FrmSobre extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnFecharActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFecharActionPerformed
         dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnFecharActionPerformed
 
-    private void jLabel8MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel8MouseClicked
+    private void lblHospedagemMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblHospedagemMouseClicked
         try {
-            Desktop.getDesktop().browse(new URI("https://github.com/cslclaman/AlogInterpreter"));
-        } catch (Exception ex) {
-            
+            Desktop.getDesktop().browse(new URI(URL_HOSPEDAGEM));
+        } catch (IOException | URISyntaxException ex) {
+            Logger.getLogger(this.getClass().getName()).log(
+                    Level.WARNING, 
+                    "{0} - {1}",
+                    new Object[]{ex.getClass().getName(), ex.getMessage()});
         }
-    }//GEN-LAST:event_jLabel8MouseClicked
+    }//GEN-LAST:event_lblHospedagemMouseClicked
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btnCopiarEmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCopiarEmailActionPerformed
         StringSelection selection;
         Clipboard clipboard;
-        selection = new StringSelection("interpretador-alog-fatecso@outlook.com.br");
+        selection = new StringSelection(URL_CONTATO_EMAIL);
         clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(selection, selection);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_btnCopiarEmailActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
+    private javax.swing.JButton btnCopiarEmail;
+    private javax.swing.JButton btnFechar;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel lblFaculdadeCurso;
+    private javax.swing.JLabel lblHospedagem;
+    private javax.swing.JLabel lblLogoFaculdade;
+    private javax.swing.JLabel lblNomeOrientador;
+    private javax.swing.JLabel lblNomeProjeto;
+    private javax.swing.JLabel lblSobreProjeto;
+    private javax.swing.JLabel lblVersao;
+    private javax.swing.JTextField txtEmail;
     // End of variables declaration//GEN-END:variables
 }
